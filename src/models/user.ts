4785241a0edb4fb0172aaaa6f8a2a63608bba7d8
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-12-02 17:59:48 -0800

    post requests for creating user

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,51 +7,54 @@ interface UserDocument {
     email: string;
     password: string;
     verified: boolean;
-    avatar?: {url: string; publicId: string}
+    avatar?: {url: string; publicId: string};
     tokens: string[];
     favourites: ObjectId[];
     followers: ObjectId[];
     following: ObjectId[];
 }
 
-const userSchema = new Schema<UserDocument>({
-    name:{
-        type: String,
-        required: true,
-        trim: true
+const userSchema = new Schema<UserDocument>(
+    {
+        name:{
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email:{
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        password:{
+            type: String,
+            required: true,
+        },
+        avatar:{
+            type: Object,
+            url: String,
+            publicId: String,
+        },
+        verified:{
+            type: Boolean,
+            default: false,
+        },
+        favourites: [{
+            type: Schema.Types.ObjectId,
+            ref: "Audio",
+        }],
+        followers: [{
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        }],
+        following: [{
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        }],
+        tokens: [String],
     },
-    email:{
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true,
-    },
-    avatar:{
-        type: Object,
-        url: String,
-        publicId: String,
-    },
-    verified:{
-        type: Boolean,
-        default: false
-    },
-    favourites: [{
-        type: Schema.Types.ObjectId,
-        ref: "Audio"
-    }],
-    followers: [{
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }],
-    following: [{
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }],
-    tokens: [String]
-}, {timestamps: true});
+    {timestamps: true}
+);
 
 export default model("User", userSchema) as Model<UserDocument>;
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,7 +1,7 @@
-import {Router} from 'express'
-import User from 'src/models/user';
+import User from '#/models/user';
+import {Router} from 'express';
 
-const router = Router()
+const router = Router();
 
 router.post('create',async  (req, res) =>{
     const {email, password, name} = req.body;
@@ -10,6 +10,6 @@ router.post('create',async  (req, res) =>{
     const user = await User.create({name, email, password});
     res.json({user});
 
-})
+});
 
 export default router;
