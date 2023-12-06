09616c401e3b4813a4c0197d0176045b7f79ddc2
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-12-06 11:06:27 -0800

    Mailtrap email verification setup

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,10 +1,28 @@
 import { CreateUser } from "#/@types/user";
 import { RequestHandler } from "express";
 import User from '#/models/user';
+import nodemailer from "nodemailer"
+import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 
-export const create: RequestHandler = (async (req: CreateUser, res) =>{
-    const {email, password, name} = req.body;
-    
-const user = await User.create({name, email, password});
-res.status(201).json({user});
+export const create: RequestHandler = (async (req: CreateUser, res) => {
+const { email, password, name } = req.body;
+
+const user = await User.create({ name, email, password });
+
+//verification email dispatch
+var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: MAILTRAP_USER,
+        pass: MAILTRAP_PASS
+    }
+});
+
+transport.sendMail({
+    to: user.email,
+    from: "auth@myapp.com",
+    html: '<h1>123456</h1>'
+})
+res.status(201).json({ user });
 })
