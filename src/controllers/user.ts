3836ade3cd0688bb7b7b7b149a3af268e3708164
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2024-01-09 12:42:03 -0800

    user email verification

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,9 @@
-import { CreateUser } from "#/@types/user";
+import { CreateUser, VerifyEmailRequest } from "#/@types/user";
 import { RequestHandler } from "express";
 import User from '#/models/user';
 import { sendVerificationMail } from "#/utils/mail";
 import { generateToken } from "#/utils/helper";
+import emailVerificationTokens from "#/models/emailVerificationTokens";
 
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
@@ -15,4 +16,24 @@ const token = generateToken()
 sendVerificationMail(token, {name, email, userId:user._id.toString() })
 
 res.status(201).json({ user: {id: user._id, name, email} });
+};
+
+export const verifyEmail: RequestHandler = async (req: VerifyEmailRequest, res) => {
+const { token, userId } = req.body;
+
+const verificationToken = await emailVerificationTokens.findOne({
+    owner: userId
+})
+
+if(!verificationToken) return res.status(403).json({error: "Invalid token!"})
+
+const matched = await verificationToken.compareToken(token)
+if(!verificationToken) return res.status(403).json({error: "Invalid token!"})
+
+await User.findByIdAndUpdate(userId, {
+    verified: true
+});
+await emailVerificationTokens.findByIdAndDelete(verificationToken._id)
+
+res.json({message: "Your email is verified."})
 };
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,13 +1,13 @@
-import { create } from '#/controllers/user';
+import { create, verifyEmail } from '#/controllers/user';
 import { validate } from '#/middleware/validator';
 import { CreateUserSchema } from '#/utils/validationSchema';
 import {Router} from 'express';
 
 const router = Router();
 
-    // router.post()
-
 router.post(
     '/create', validate(CreateUserSchema),create);
+router.post(
+    '/verify-email', verifyEmail);
 
 export default router;
