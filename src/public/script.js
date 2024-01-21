983c5b335a2288ca1f8264c58a50aa67533cb9fe
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2024-01-20 17:37:37 -0800

    reset password UI created

--- src/public/script.js
+++ src/public/script.js
@@ -1,5 +1,17 @@
-const error = document.getElementById("error");
+const getById = (id) => {
+    return document.getElementById(id);
+}
+
+const password = getById('password')
+const confirmPassword = getById('confirm-password')
+const form = getById('form')
+const container = getById('container')
+const loader = getById('loader')
+const button = getById('submit')
+const error = getById("error");
 const success = document.getElementById("success");
+
+
 
 error.style.display = "none";
 success.style.display = "none";
