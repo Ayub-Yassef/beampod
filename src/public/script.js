68405408188404a352f55120e3c38368f38c74c2
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2024-01-21 15:26:04 -0800

    validation token

--- src/public/script.js
+++ src/public/script.js
@@ -11,7 +11,37 @@ const button = getById('submit')
 const error = getById("error");
 const success = document.getElementById("success");
 
+let token, userId;
 
+window.addEventListener('DOMContentLoaded', async () => {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => {
+            return searchParams.get(prop);
+        },
+    });
+token = params.token
+userId = params.userId
 
+const res = await fetch("/auth/verify-pass-reset-token",
+{
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+        token, userId
+    })
+})
+
+if(!res.ok){
+    const { error } = await res.json()
+    loader.innerText = error
+    return
+}
+
+loader.style.display = "none";
+container.style.display = "block"
+
+});
 error.style.display = "none";
 success.style.display = "none";
