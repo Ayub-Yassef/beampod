bb8daf2bd8b53280a03a8a4f3ade6ceb205c2203
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2024-01-26 12:27:00 -0800

    mustAuth method added to middleware

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,5 +1,5 @@
 import { create, generateForgotPasswordLink, grantValid, sendReVerificationToken, signIn, updatePassword, verifyEmail } from '#/controllers/user';
-import { isValidPassResetToken } from '#/middleware/auth';
+import { isValidPassResetToken, mustAuth } from '#/middleware/auth';
 import { validate } from '#/middleware/validator';
 import User from '#/models/user';
 import { CreateUserSchema, SignInValidationSchema, TokenAndIdValidation, UpdatePasswordSchema } from '#/utils/validationSchema';
@@ -23,28 +23,12 @@ router.post(
     '/update-password', validate(UpdatePasswordSchema), isValidPassResetToken, updatePassword)
 router.post(
     '/sign-in', validate(SignInValidationSchema), signIn)
-router.get('/is-auth', async (req, res) => {
-    const { authorization } = req.headers;
-    const token = authorization?.split("Bearer ")[1];
-    if (!token) return res.status(403).json({ error: "Unauthorised request."});
-
-    const payload = verify(token, JWT_SECRET) as JwtPayload;
-    const id = payload.userId;
-
-    const user = await User.findById(id);
-    if (!user) return res.status(403).json({ error: "Unauthorised request" });
+router.get('/is-auth', mustAuth, (req, res) => {
 
     res.json({
-        profile: {
-            id: user._id,
-            name: user.name,
-            email: user.email,
-            verified: user.verified,
-            avatar: user.avatar?.url,
-            followers: user.followers.length,
-            following: user.following.length,
-        },
-    });
-});
+        profile: req.user,
+        });
+    }
+);
 
 export default router;
